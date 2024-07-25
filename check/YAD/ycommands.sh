9e2a05775b305fb4cd695057a3a3745e58c9895f
batann <122584289+batann@users.noreply.github.com>
2024-07-26 00:54:40 +1000

    Add files via upload

---
+++ check/YAD/ycommands.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+yad --title "APT REPOS" --form --borders=10 --columns=2 --image-on-top --image="/home/batan/Pictures/wall.166.png" \
+--field="clean":fbtn "sudo -u batan bash clean.sh"  \
+--field="count1":fbtn "sudo -u batan bash count1.sh"  \
+--field="downm":fbtn "sudo -u batan bash downm.sh"  \
+--field="downv":fbtn "sudo -u batan bash downv.sh"  \
+--field="fstab":fbtn "sudo -u batan bash fstab.sh"  \
+--field="megals":fbtn "sudo -u batan bash megals.sh"  \
+--field="megastorage":fbtn "sudo -u batan bash megastorage.sh"  \
+--field="metadata":fbtn "sudo -u batan bash metadata.sh"  \
+--field="nuke":fbtn "sudo -u batan bash nuke.sh"  \
+--field="popup":fbtn "sudo -u batan bash popup.sh"  \
+--field="psaux":fbtn "sudo -u batan bash psaux.sh"  \
+--field="scplocal":fbtn "sudo -u batan bash scplocal.sh"  \
+--field="setssh":fbtn "sudo -u batan bash setssh.sh"  \
+--field="shred":fbtn "sudo -u batan bash shred.sh"  \
+--field="sssh":fbtn "sudo -u batan bash sssh.sh"  \
+--field="start_right":fbtn "sudo -u batan bash start_right.sh"  \
+--field="usb":fbtn "sudo -u batan bash usb.sh"  \
---
+++ check/YAD/ysettings.sh
@@ -0,0 +1,21 @@
+
+
+yad --text="SETTINGS" --image='/home/batan/Pictures/yad.png' --borders=10 --columns=4 --form \
+--field="caps":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash caps.sh'"  \
+--field="custom-grub":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash custom-grub.sh'"  \
+--field="firewall":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash firewall.sh'"  \
+--field="fonts":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash fonts.sh'"  \
+--field="fstab":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash fstab.sh'"  \
+--field="gpg":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash gpg.sh'"  \
+--field="keybinding":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash keybinding.sh'"  \
+--field="lamp":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash lamp.t.sh'"  \
+--field="mega":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash mega.sh'"  \
+--field="powermanager":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash powermanager.sh'"  \
+--field="snapwindow":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash snapwindow.sh'"  \
+--field="ssh":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash ssh.sh'"  \
+--field="superkey":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash superkey.sh'"  \
+--field="themes":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash themes.sh'"  \
+--field="timezone":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash timezone.sh'"  \
+--field="visudo":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash visudo.sh'"  \
+--field="yvimSETUP VIM":fbtn "xterm -geom 150x10-0-0 -e bash -c 'sudo -u batan bash yvim.sh'" \
+
